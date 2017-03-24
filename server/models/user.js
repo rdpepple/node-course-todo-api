a2d57674770a80baa0009c791b1c0e850daffad7
Robert Pepple <bob.pepple@comcast.net>
2017-03-24 15:59:03 -0700

    Add GET /users/me and authentication middleware

--- server/models/user.js
+++ server/models/user.js
@@ -7,6 +7,7 @@ var UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
+    isAsync:true,
     minlength: 1,
     trim: true,
     unique: true,
@@ -48,6 +49,23 @@ UserSchema.methods.generateAuthToken = function () {
 
   return user.save().then(() => {
     return token;
+  });
+};
+
+UserSchema.statics.findByToken = function (token) {
+  var User = this;
+  var decoded;
+
+  try {
+    decoded = jwt.verify(token, 'abc123');
+  } catch (e) {
+    return Promise.reject();
+  }
+
+  return User.findOne({
+    '_id': decoded._id,
+    'tokens.token': token,
+    'tokens.access': 'auth'
   });
 };
 

b6d5084512dbbbf7d827229bb45c44a832b6f578
Robert Pepple <bob.pepple@comcast.net>
2017-03-25 16:50:36 -0700

    Add POST /user/login route

--- server/models/user.js
+++ server/models/user.js
@@ -3,6 +3,7 @@ const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
 const bcrypt = require('bcryptjs');
+const {SHA256} = require('crypto-js');
 
 var UserSchema = new mongoose.Schema({
   email: {
@@ -67,6 +68,26 @@ UserSchema.statics.findByToken = function (token) {
     '_id': decoded._id,
     'tokens.token': token,
     'tokens.access': 'auth'
+  });
+};
+
+UserSchema.statics.findByCredentials = function (email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
   });
 };
 
